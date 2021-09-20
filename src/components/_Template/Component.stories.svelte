a80a78335b2fa56758d8b60988ed839c27057c36
Ruben La Biunda <ruben.labiunda@stellar.de>
2021-09-20 20:26:15 +0200

    adds template component story

---
+++ src/components/_Template/Component.stories.svelte
@@ -0,0 +1,31 @@
+<script>
+  import { Meta, Template, Story } from "@storybook/addon-svelte-csf";
+  import Component from "./Component.svelte";
+</script>
+
+<Meta
+  title="Example/Component"
+  component={Component}
+  argTypes={{
+    isModified: {control: "boolean"},
+  }}
+/>
+
+<Story name="Overview" let:args>
+  <Component></Component>
+  <Component isModified></Component>
+  <hr/>
+  <Component {...args}></Component>
+</Story>
+
+<!-- Individual stories -->
+
+<Template let:args>
+  <Component {...args} />
+</Template>
+
+<Story name="Normal" />
+
+<Story name="Modified" args={{
+  isModified: true,
+}}/>
---
+++ src/components/_Template/Component.svelte
@@ -0,0 +1,18 @@
+<script>
+   export let isModified = false
+</script>
+
+<h1 class="Component"
+  class:__modified={isModified}>
+  Component
+</h1>
+
+<style>
+  .Component {
+    color: snow;
+    background: rebeccapurple;
+  }
+  .Component.__modified {
+      background: indianred;
+  }
+</style>
