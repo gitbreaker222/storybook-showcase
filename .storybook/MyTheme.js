77b94f6a6507223e221e131d45031893fa1665b9
Ruben La Biunda <ruben.labiunda@stellar.de>
2021-09-22 17:44:22 +0200

    updates theme

--- .storybook/MyTheme.js
+++ .storybook/MyTheme.js
@@ -2,7 +2,36 @@ import { create } from '@storybook/theming';
 import Logo from './logo kopfhörer.jpg'
 
 export default create({
+    // Color
     base: 'dark',
+    colorPrimary: 'yellow',
+    colorSecondary: 'hotpink',
+
+    // UI
+    // Text colors
+    textColor: 'snow',
+    textInverseColor: '#222',
+    appBg: 'rebeccapurple',
+    appContentBg: 'white',
+    appBorderColor: 'silver',
+    appBorderRadius: 4,
+
+    // Typography
+    fontBase: '"Open Sans", sans-serif',
+    fontCode: 'monospace',
+
+    // Toolbar default and active colors
+    barBg: 'rebeccapurple',
+    barTextColor: 'snow',
+    barSelectedColor: 'snow',
+
+    // Form colors
+    inputBg: 'snow',
+    inputTextColor: 'black',
+    inputBorder: 'silver',
+    inputBorderRadius: 4,
+
+    // Branding
     brandTitle: 'My storybook showcase',
     brandUrl: 'https://www.rubenlabiunda.de',
     brandImage: Logo,
