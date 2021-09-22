274b61ef4bc3e2108fc34ea111d22329e43e5c01
Ruben La Biunda <ruben.labiunda@stellar.de>
2021-09-22 16:12:55 +0200

    inits custom theme

---
+++ .storybook/MyTheme.js
@@ -0,0 +1,9 @@
+import { create } from '@storybook/theming';
+import Logo from './logo kopfhörer.jpg'
+
+export default create({
+    base: 'dark',
+    brandTitle: 'My storybook showcase',
+    brandUrl: 'https://www.rubenlabiunda.de',
+    brandImage: Logo,
+})
