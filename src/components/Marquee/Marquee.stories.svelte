bf1a9b1fdfa0c0de8d78d9549c21b2553ac11c1a
Ruben L <dummy@email.address>
2021-09-24 00:11:53 +0200

    inits marquee component

---
+++ src/components/Marquee/Marquee.stories.svelte
@@ -0,0 +1,44 @@
+<script>
+  import { Meta, Template, Story } from '@storybook/addon-svelte-csf';
+  import Marquee from './Marquee.svelte';
+</script>
+
+<Meta
+  title="Example/Marquee"
+  component={Marquee}
+  argTypes={{
+    slot: { control: 'text' },
+    speed: { control: 'number' },
+  }}
+  args={{
+    slot: 'Marquee',
+  }}
+/>
+
+<Story name="Overview" let:args>
+  <div><code>Overview</code></div>
+  <Marquee />
+  <Marquee>short text</Marquee>
+  <Marquee
+    >Lorem ipsum dolor, sit amet consectetur adipisicing elit. Laborum atque
+    explicabo ea fuga vitae placeat blanditiis incidunt eius consequuntur.
+    Accusamus nisi omnis earum, iure non esse soluta et ex ipsam.</Marquee
+  >
+  <div><code>Interactive Demo</code></div>
+  <Marquee {...args}>{args.slot}</Marquee>
+</Story>
+
+<!-- Individual stories -->
+
+<Template let:args>
+  <Marquee {...args} />
+</Template>
+
+<Story name="Normal" />
+
+<Story
+  name="Modified"
+  args={{
+    speed: 100,
+  }}
+/>
---
+++ src/components/Marquee/Marquee.svelte
@@ -0,0 +1,51 @@
+<script>
+  import { onMount } from 'svelte';
+
+  let canvasElement;
+
+  export let speed = 60; //px per s
+  let train;
+
+  onMount(() => {
+    const offsetStart = train.parentNode.clientWidth;
+    const offsetEnd = train.scrollWidth;
+    const duration = (offsetStart + offsetEnd) / speed;
+    train.style.setProperty('--offsetStart', `${offsetStart}px`);
+    train.style.setProperty('--offsetEnd', `${-offsetEnd}px`);
+    train.style.setProperty('--durationThrough', `${duration}s`);
+  });
+</script>
+
+<div class="rail">
+  <div class="train regular" bind:this={train}><slot /></div>
+</div>
+
+<style>
+  .rail {
+    height: 3em;
+    width: 25ch;
+    display: flex;
+    align-items: center;
+    white-space: nowrap;
+    overflow: hidden;
+    box-shadow: 0 0 2px gray inset;
+    position: relative;
+  }
+
+  .rail .train.regular {
+    --offsetStart: 0;
+    --offsetEnd: -100%;
+    --durationThrough: 10s;
+    animation: run-through linear infinite;
+    animation-duration: var(--durationThrough);
+  }
+
+  @keyframes run-through {
+    0% {
+      transform: translateX(var(--offsetStart));
+    }
+    100% {
+      transform: translateX(var(--offsetEnd));
+    }
+  }
+</style>
